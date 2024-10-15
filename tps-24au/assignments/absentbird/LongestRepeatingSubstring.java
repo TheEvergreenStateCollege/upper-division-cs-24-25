c073b6bee7861122425a907ca79e49461fb0ac0a
Shawn Bird <absent.bird@gmail.com>
2024-10-14 17:43:18 -0700

    improved answer

--- tps-24au/assignments/absentbird/LongestRepeatingSubstring.java
+++ tps-24au/assignments/absentbird/LongestRepeatingSubstring.java
@@ -1,11 +1,12 @@
 class LongestRepeatingSubstring {
     public static String longestRepeatingSubstring(String s) {
         int half = s.length() / 2;
-        for(int i = 0; i < s.length(); i++) {
+        String longest = "";
+        for(int i = 0; i < s.length() - 1; i++) {
             if (i % 2 == 1) {
                 half--;
             }
-            if (s.length() % 2 == 1) {
+            if (i < 3 && s.length() % 2 == 1) {
                 if (i == 1) {
                     half ++;
                 } else if (i == 2) {
@@ -14,11 +15,16 @@ class LongestRepeatingSubstring {
             }
             String candidate = s.substring(i, i + half);
             String next = s.substring(i + half, i + half * 2);
-            if (candidate.equals(next)) {
-                return candidate;
+            if (candidate.equals(next) && candidate.length() > longest.length()) {
+                longest = candidate;
+            } else if (half > 0) {
+                String halfcheck = longestRepeatingSubstring(candidate);
+                if (halfcheck.length() > longest.length()) {
+                    longest = halfcheck;
+                }
             }
         };
-        return "";
+        return longest;
     }
     public static void main(String []args) {
         String result = longestRepeatingSubstring(args[0]);
