6868ae1d6fb41ec85a60ac8174c64b6c844838b4
Paul Pham <148553+learner-long-life@users.noreply.github.com>
2024-10-17 23:34:55 +0000

    Add a script to clone CSInParallel source files and ignore it, to prevent accidental commits.

---
+++ pdc-24au/gh-csinparallel.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+# Run this script to clone all the source from the textbook
+# https://github.com/csinparallel/CSinParallel
+git clone https://github.com/csinparallel/CSinParallel.git
