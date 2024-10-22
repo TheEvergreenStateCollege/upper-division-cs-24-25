aa3e5be98dc01de4b77787b7453a7bdd4ccc60b5
Daniel Monroe <dtmonroe95@gmail.com>
2024-10-22 19:07:40 +0000

    git hub is complicated

--- dsa-24au/week-04/dl-dictionary.sh
+++ dsa-24au/week-04/dl-dictionary.sh
@@ -2,5 +2,6 @@
 
 FILENAME="data/pg29765.txt"
 if [ ! -e "${FILENAME}" ]; then
-    wget https://www.gutenberg.org/cache/epub/29765/pg29765.txt -o ${FILENAME}
+    wget https://www.gutenberg.org/cache/epub/29765/pg29765.txt 
 fi
+
