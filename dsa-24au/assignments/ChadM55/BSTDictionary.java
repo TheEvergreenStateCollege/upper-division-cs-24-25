6b15894fcafce568b1af129fed5d54f55368ae9b
Chad Martin <chad.m.mart@gmail.com>
2024-11-04 20:04:05 +0000

    I added a blank board state files

--- dsa-24au/assignments/ChadM55/BSTDictionary.java
+++ dsa-24au/assignments/ChadM55/BSTDictionary.java
@@ -20,8 +20,17 @@ public class BSTDictionary implements Dictionary {
     }
 
     public List<String> lookup(String term) {
-
         // TODO replace this
+        int low = 0;
+        int high = array.length - 1;
+        int mid = low + (high - low)/2;
+
+        exList = [0, 1, 2, 3, 4]
+        key = 4
+
+        if (high == low) {
+            return List[low];
+        }
         return List.of();
 
    }
--- pdc-24au/mandelbrot.c
+++ pdc-24au/mandelbrot.c
@@ -6,32 +6,31 @@
 *
 */
 
-#include <math.h>   
+#include <math.h> 
 #include <stdio.h>
 #include <complex.h>
 #include <stdlib.h>
+#include <time.h>
 
 
-int size = 400;
+int size = 1000;
 int **image;
 int *img_data;
 
 void create_image() {
   img_data = (int *) malloc(size * size * sizeof(int));
   image = (int **) malloc(size * sizeof(int *));
-}
 
-int main () {
-  // alocate image and image_data
-  create_image();
-  
-  // loop through image and compute color
+  for(int r = 0; r < size; r++) {
+    image[r] = img_data + r * size;
+  }
 }
 
 /// row = y coord, i.e. imaginary
 /// col = x coord, i.e. real
 /// <param name="MaxCount">max iterations of loop before giving up</param>
 /// <returns> number of iterations</returns>
+//
 int kernel(int row, int col, int MaxCount) {
     int i;
     double complex z = 0;
@@ -44,3 +43,47 @@ int kernel(int row, int col, int MaxCount) {
     printf("%f + %f * i\n", creal(z), cimag(z));
     return i;
 }
+
+void printPixels(int *pixelBoard) {
+  int count = 0;
+  /*
+  for(int i = 0; i < size; i++) {
+    for(int j = 0; j < size; j++) {
+      if (count % 10 == 0) {
+        printf("\n");
+      }
+      printf("%d", pixelBoard[i][j]);
+      count++;
+    }
+  }*/
+
+  for (int i = 0; i < size; i++) {
+    if (count % 10 == 0) {
+      printf("\n");
+    }
+    printf("%-3d ", pixelBoard[i]);
+    count++;
+  }
+}
+
+int main () {
+  clock_t begin = clock();
+  // alocate image and image_data
+  create_image();
+  int count = 0;
+  // loop through image and compute color
+  // call kernel at every point of a nested for loop
+  #pragma omp parallel for //collapse (2)
+  for (int i = 0; i < size; i++) {
+    //int num = omp_get_thread_num();
+    //printf("ID: %d", num);
+    for (int j = 0; j < size; j++) {
+      image[i][j] = kernel(i, j, size);
+    }
+  }
+  printPixels(img_data);
+  clock_t end = clock();
+  double time_spent = (double)(end - begin)/ (double)(CLOCKS_PER_SEC);
+  printf("\n%lf\n", time_spent);
+}
+
