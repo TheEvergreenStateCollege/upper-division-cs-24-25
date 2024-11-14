d1c6408cf3164f207356a79384d8b26c35f339b4
EvergreenTor <146137721+EvergreenTor@users.noreply.github.com>
2024-11-14 20:43:00 +0000

    Paul and I made progress on a python implementation of prefix tree and also fixed the java version a little

--- tps-24au/assignments/EvergreenTor/tps_week_6/prefix_tree.py
+++ tps-24au/assignments/EvergreenTor/tps_week_6/prefix_tree.py
@@ -6,10 +6,44 @@ class Node:
     def addNode(self,letter):
         new_node = Node(self)
         self.children[letter] = new_node
+
+    def insertChild(self, suffix):
+         
+        if len(suffix) == 0:
+            return False 
+        
+        c = suffix[0]
+        inserted = False
+
+        # n is either existing child for character c or a new child
+
+        n = Node
+
+        if c in self.children:
+            n = self.children[c]
+        else:
+            n = Node(self)
+            self.children[c] = n
+            
+            inserted = n.insertChild(suffix[1:])
+        return inserted 
         
 class PrefixTree:
     def __init__(self):
         self.root = Node(None)
+
+    
+
+
+        
+
+        
+
+
+
+
+
+
         
     
     
