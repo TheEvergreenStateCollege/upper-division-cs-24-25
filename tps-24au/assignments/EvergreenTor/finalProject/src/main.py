065df6702dca73f4b2747221180a45c08d7bd6d2
EvergreenTor <146137721+EvergreenTor@users.noreply.github.com>
2024-12-09 21:10:35 +0000

    project work

--- tps-24au/assignments/EvergreenTor/finalProject/src/main.py
+++ tps-24au/assignments/EvergreenTor/finalProject/src/main.py
@@ -1,18 +1,14 @@
-from testMaxHeap import MaxHeap
+from max_heap import MaxHeap
 
 heap = MaxHeap()
 
-player1 = {'name': 'Johnathan Joestar', 'goals_scored': 4}
-player2 = {'name': 'Joseph Joestar', 'goals_scored': 3}
-player3 = {'name': 'Kujo Jotaro', 'goals_scored': 12}
 
-heap.push(player1)
-heap.push(player2)
-heap.push(player3)
+
+heap.push(4)
+heap.push(34)
+heap.push(12)
+heap.push(50)
 
 
 if __name__ == "__main__":
-    print(heap.peek())
-    print(heap.size())
-
-    print(str(heap))
+    print(heap.heap)
