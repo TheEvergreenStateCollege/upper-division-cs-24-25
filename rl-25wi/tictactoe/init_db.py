14c9e6d4838f975818ef98e9818f8d8568ac328f
Acstrayer <nskkid@gmail.com>
2025-01-14 00:31:37 -0800

    Added player cookies to matches schema and reinitialized database

--- rl-25wi/tictactoe/init_db.py
+++ rl-25wi/tictactoe/init_db.py
@@ -5,7 +5,10 @@ connection = sqlite3.connect("database.db")
 with open("matches.sql") as f:
     connection.executescript(f.read())
 cur = connection.cursor()
-cur.execute("INSERT INTO matches (matchID) VALUES(?)", (["s0NlC"]))
+cur.execute(
+    "INSERT INTO matches (player1, player2, matchID) VALUES (?, ?, ?)",
+    ("cookie1", "cookie2", "s0NlC"),
+)
 connection.commit()
 connection.close()
 
