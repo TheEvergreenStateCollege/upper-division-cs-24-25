abd603e7f68d08c640a7fd8b16b429bd036bb0df
Shawn Bird <absent.bird@gmail.com>
2025-02-06 17:02:57 -0800

    added lab5 progress

--- rl-25wi/shawn/lab5.py
+++ rl-25wi/shawn/lab5.py
@@ -8,6 +8,7 @@ class Hand:
     def __init__(self):
         self.score = 0
         self.turn = 0
+        self.hits = 0
         self.usable_ace = False
         self.playing = True
         self.bust = False
@@ -30,14 +31,13 @@ class Hand:
        self.turn += 1
        self.playing = False
 
- class Dealer:
+class Dealer:
     def __init__(self):
         self.hand = Hand()
         self.hand.hit()
         self.show = self.hand.score
         self.hand.hit()
-    def next_turn(self):
-        if self.hand.score < 17:
+        while self.hand.score < 17:
             self.hand.hit()
         else:
             self.hand.stick()
@@ -46,28 +46,58 @@ class Agent:
     def __init__(self):
         self.new_episode()
         self.episodes = []
+        self.states = {}
     def new_episode(self):
         self.episode = []
         self.hand = Hand()
         self.hand.hit()
         self.hand.hit()
         self.dealer = Dealer()
-        if self.hand.score < 12:
+        while self.hand.score < 12:
             self.hand.hit()
         state = self.State(self.hand, self.dealer.show)
         self.episode.append(state)
     def play_hand(self):
         while self.hand.playing:
-
             state = self.State(self.hand, self.dealer.show)
-            self.episode.append(state)
+            if state.state_id() not in self.states:
+                self.states[state.state_id()] = state
+            else:
+                self.states[state.state_id()].visited += 1
+            self.episode.append(self.states[state.state_id()])
+            if self.hand.score < 20:
+                self.hand.hit()
+            else:
+                self.hand.stick()
         self.end_episode()
     def end_episode(self):
+        # 0 = Draw, -1 = Lose, 1 = Win
+        reward = 0
+        if self.hand.bust:
+            reward = -1
+        elif self.hand.score < self.dealer.hand.score and not self.dealer.hand.bust:
+            reward = -1
+        elif self.dealer.hand.bust or self.hand.score > self.dealer.hand.score:
+            reward = 1
+        for i in range(len(self.episode) - 1, -1, -1):
+            state = self.episode[i]
+            ot = state.estimate * (state.visited - 1)
+            state.estimate = (ot + reward) / state.visited
+        self.episodes.append(self.episode)
+        self.new_episode()
     class State:
         def __init__(self, hand, upcard):
             self.hand = hand
             self.upcard = upcard
+            self.visited = 1
+            self.estimate = 0.0
+        def state_id(self):
+            return (self.hand.score, self.upcard, self.hand.usable_ace)
+        def __str__(self):
+            return "Hand: " + str(self.hand.score) + " |  Upcard: " + str(self.upcard) + " | Usable Ace: " + str(self.hand.usable_ace) + " | Estimate: " + str(round(self.estimate, 2)) + " | Visited: " + str(self.visited)
 
 agent = Agent()
 while len(agent.episodes) < 1000:
     agent.play_hand()
+for s in agent.states.values():
+    print(s)
