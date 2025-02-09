32485449f9468aaacc8826316bf45b1a955fb4fe
Shawn Bird <absent.bird@gmail.com>
2025-02-08 22:36:31 -0800

    updated lab5

---
+++ dof-25wi/notes-1-9.md
@@ -0,0 +1,55 @@
+Github overview:
+- What is git?
+- How to open an issue/become a maintainer
+
+SMART Goals: How to write useful/measurable goals.
+Specific, Measurable, Actionable, Realistic, Time-bound
+
+Week 1 discussions and how to discuss them while facilitating.
+
+Learning Contract: due next Tuesday. Use SMART goals.
+
+Writing on Canvas every week. Write about work done with team.
+
+Lead 1 hour learning activity each week.
+
+Minimum viable product:
+- Internet at farm hub?
+- Scale measurements?
+- 100ms ping goal?
+- Hooked up to solar power?
+- Different goals for different teams?
+
+Take measurements at farm: define maximum distances and areas.
+
+Figure out what work can be done before parts arrive.
+
+Stand-up meeting each class to explore barriers and track progress.
+
+---
+
+Organize the fuck out of everything, so hard.
+
+Create a maintenance document: part of MVP
+
+Process -> process -> big badass goal
+
+Goals:
+- Wirless connectivity at wash station
+- Solar power for the wash station
+- Maintenance documents
+- Process documentation
+- Weigh station
+- Management panel
+- Data entry panel
+- API & Authentication
+- 
+
+Stretch goals:
+- Hexepod robot
+- IOT Mesh using ESP32 for moisture sensors
+- Automate scales & data collection
+- Point of sale integration
+- E-commerce website
+- Unify data flows: clover, pos, etc.
+- Weather station?
--- rl-25wi/shawn/lab5.py
+++ rl-25wi/shawn/lab5.py
@@ -1,10 +1,34 @@
-import random
-from tabulate import tabulate
+#!/bin/python3
+import sys # For reading cli arguments
+import random # For drawing cards
+from tabulate import tabulate # For pretty printing of policy
 
+# Group:
+# Shawn Bird
+# Austin Strayer
+# Hayden Edge
+# Dawson White
+
+## RL Lab 5
+## Goals: understand Monte Carlo simulation, in particular Blackjack
+## Task: program the simulation in Python, where the agent and the 
+## environment are distinct. The agent estimates the value of a state
+## by averaging the returns using first visit.
+## A state is the sum of values in a hand plus the value of the 
+## dealer's card showing plus whether there is a usable ace.
+## There are about 200 states.
+## 1. build the environment
+## 2. build a simple agent
+## 3. collect data
+## Work in groups and make sure  include the names of the members in your group
+
+# Create a list of possible card draws, a 10 for each face card
 card_list = [1,2,3,4,5,6,7,8,9,10,10,10,10]
 def draw_card():
     return random.choice(card_list)
 
+# Hands hold the state relative to the player. Agents will have a hand and so will the dealer
+# The hit() and stick() methods allow the hand to be played
 class Hand:
     def __init__(self):
         self.score = 0
@@ -31,6 +55,8 @@ class Hand:
        self.playing = False
        self.turn += 1
 
+# Dealer class to hold dealer behavior
+# By default the dealer will hit until they have a score of 17 or more
 class Dealer:
     def __init__(self):
         self.hand = Hand()
@@ -42,11 +68,14 @@ class Dealer:
         else:
             self.hand.stick()
 
+# Class to hold the agent, including episodes and states
 class Agent:
     def __init__(self):
         self.episodes = []
         self.states = {}
         self.new_episode()
+    # Episodes are used to segment hands
+    # new_episode() resets the hand and hits until a score of 12 or more
     def new_episode(self):
         self.episode = []
         self.hand = Hand()
@@ -62,11 +91,13 @@ class Agent:
         else:
             self.states[state.state_id()].visited += 1
     def play_hand(self):
+        # By default the agent will hit until reaching a score of 20 or greater
         while self.hand.playing:
             if self.hand.score < 20:
                 self.hand.hit()
             else:
                 self.hand.stick()
+            # Add to the visited counter for each state reached, starts at 1
             state = self.State(self.hand.score, self.dealer.show, self.hand.usable_ace)
             if state.state_id() not in self.states:
                 self.states[state.state_id()] = state
@@ -75,20 +106,29 @@ class Agent:
             self.episode.append(self.states[state.state_id()])
         self.end_episode()
     def end_episode(self):
+        # Calculate the reward
         # 0 = Draw, -1 = Lose, 1 = Win
         reward = 0
+        # Busting always results in a loss
         if self.hand.bust:
             reward = -1
+        # Getting a lower score than the dealer also results in a loss
         elif self.hand.score < self.dealer.hand.score and not self.dealer.hand.bust:
             reward = -1
+        # If the dealer busts or has a lower score it results in a win
         elif self.dealer.hand.bust or self.hand.score > self.dealer.hand.score:
             reward = 1
+        self.episode[-1].outcome = reward
+        # All other conditions result in a draw
+        # Add the reward to the average for each state in the episode
         for i in range(len(self.episode) - 1, -1, -1):
             state = self.episode[i]
             ot = state.estimate * (state.visited - 1)
             state.estimate = (ot + reward) / state.visited
         self.episodes.append(self.episode)
+        # Start the next episode and draw a fresh hand
         self.new_episode()
+    # Class to hold the states used for calculating the policy
     class State:
         def __init__(self, score, upcard, ace):
             self.score = score
@@ -96,14 +136,41 @@ class Agent:
             self.upcard = upcard
             self.visited = 1
             self.estimate = 0.0
+            self.outcome = 0
         def state_id(self):
             return (self.score, self.upcard, self.usable_ace)
 
+hands = 100 # Default value
+# Check first argument for custom number of hands
+if len(sys.argv) > 1 and sys.argv[1].isdigit():
+    hands = int(sys.argv[1])
+
+# Initialize the agent
 agent = Agent()
-while len(agent.episodes) < 1000:
+# Play  hands
+while len(agent.episodes) < hands:
     agent.play_hand()
-vals = []
+
+vals = [] # To store the policy as a two dimensional array
+# Load each state into vals
 for s in agent.states.values():
     vals.append([s.score, s.upcard, s.usable_ace, round(s.estimate, 2), s.visited])
+
+# Sort by ending score first, then by upcard
 vals.sort(key=lambda x: (x[0], x[1]))
+# Print the tabulated data with headers
 print(tabulate(vals, headers=['Hand', 'Upcard', 'Ace', 'Estimate', 'Visited']))
+print() # Blank line for spacing
+
+# Count up the total wins, losses and draws then print them
+wins = 0
+losses = 0
+draws = 0
+for e in agent.episodes:
+    if e[-1].outcome == 1:
+        wins += 1
+    elif e[-1].outcome == -1:
+        losses += 1
+    else:
+        draws += 1
+print("Wins: {0:3d} | Losses: {1:3d} | Draws: {2:3d}".format(wins, losses, draws))
