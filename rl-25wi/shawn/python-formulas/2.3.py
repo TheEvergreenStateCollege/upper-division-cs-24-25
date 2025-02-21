379f68c591aa996e6ecbf3f101b58c67647aa340
Shawn Bird <absent.bird@gmail.com>
2025-02-20 20:55:08 -0800

    switched to generator function

--- rl-25wi/shawn/python-formulas/2.3.py
+++ rl-25wi/shawn/python-formulas/2.3.py
@@ -1,17 +1,20 @@
 # Temporal Difference Function
-def get_estimate(reward=0, expectation=0, iteration=1):
-    # Difference between current reward and expectation
-    difference = reward - expectation
-    # Proportion of this iteration to the total
-    ratio = 1 / iteration
-    # New estimate = old estimate + proportional difference
-    return expectation + ratio * difference
+def get_estimates(rewards=[], estimate=0):
+    iteration = 1
+    # Loop over the rewards, adjusting the estimate each time
+    for reward in rewards:
+        # Proportion of this iteration to the total
+        ratio = 1 / iteration
+        # Difference between current reward and expectation
+        difference = reward - estimate
+        # New estimate = old estimate + proportional difference
+        estimate += ratio * difference
+        yield estimate
+        iteration += 1
 
 # Starting Inputs:
 sample_rewards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-estimate = 0
+starting_value = 0 # Starting estimate
 
-# Loop over rewards, adjusting the estimate each time
-for iteration, reward in enumerate(sample_rewards, start=1):
-    estimate = get_estimate(reward, estimate, iteration)
+for estimate in get_estimates(sample_rewards, starting_value):
     print(estimate) # Print to show progress
--- rl-25wi/shawn/python-formulas/3.19.py
+++ rl-25wi/shawn/python-formulas/3.19.py
@@ -1,23 +1,43 @@
 # Bellman Optimality Equation Function
-def optimal_value(state, depth=1):
+def optimal_value(state, depth=0):
     # Set to negative infinity to be replaced on first comparison
     max_value = float('-inf')
+    if depth < 1:
+        return rewards[state] # Base case
+    # Compare the value of each possible action
+    probability = 1 / len(actions)
     for action in actions:
+        if state + action not in states:
+            continue
         state_value = 0
         for state_prime in states:
-            for reward in rewards:
-                ratio = probability(state_prime, reward, state, action)
-                optimal = optimal_value(state_prime, depth-1)
-                state_value += discount * probability * optimal
+            reward = rewards[index]
+            optimal = optimal_value(state_prime, depth-1)
+            state_value += discount * probability * optimal
         # Keep track of the largest estimate
-        max_value = max(max_value, state_estimate)
+        max_value = max(max_value, state_value)
     return max_value
 
 # Starting Inputs:
-states = [-1, 2, 5, 10] # Policy evaluation of actions
-rewards = [1, 0, 0, -1] # Reward from current state
+states = range(5) # List of states
+rewards = [1, 0, 0, 0, -1] # Reward locations
+actions = [-1, 1] # Move left, move right
 discount = 0.9 # Falloff over distance
-depth = 6 # Recursive depth of calculations
+depth = 4 # Recursive depth of calculations
+
+# Dictionary of each action at each state
+policy = {}
+# Build policy from actions and states
+# (state, action): (next_state, reward)
+for state in states:
+    for action in actions:
+        identifier = (state, action)
+        next_state = state + action
+        if next_state not in states:
+            policy[identifier] = None
+            continue
+        probability = rewards[next_state]
+        policy[identifier] = (next_state, reward)
 
 for state in states:
     optimal = optimal_value(state, depth)
