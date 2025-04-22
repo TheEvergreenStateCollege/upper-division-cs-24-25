3ca9a2c4fde3f85d2607b992c7f522de971dd642
Paul Pham <148553+learner-long-life@users.noreply.github.com>
2025-04-22 23:43:40 +0000

    Add a script to download Apache spark and gitignore its tarball.

--- scripts/dl-spark.sh
+++ scripts/dl-spark.sh
@@ -1,6 +1,8 @@
-#!/usr/bin/env
+#!/usr/bin/env sh
 
 # Debug mode to see all the file downloading and extracting
 set -x
-RUN wget https://www.apache.org/dyn/closer.lua/spark/spark-3.5.5/spark-3.5.5-bin-hadoop3.tgz
-bin/spark-shell --packages graphframes:graphframes:0.8.4-spark3.5-s_2.12
+wget // need to copy and paste this externall
+zcat spark-3.5.5-bin-hadoop3.tgz | tar xvf -
+mv spark-3.5.5 /opt/spark-3.5.5-bin-hadoop
+# bin/spark-shell --packages graphframes:graphframes:0.8.4-spark3.5-s_2.12
